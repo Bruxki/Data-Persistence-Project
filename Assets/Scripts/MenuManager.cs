f40e349b7e747568fef3571582e155abd53db3cc
Bruh <177808919+Bruxki@users.noreply.github.com>
2025-05-06 15:56:07 +0600

    Added functions for save and load

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -8,16 +9,74 @@ public class MenuManager : MonoBehaviour
 {
     public static MenuManager Instance;
 
+    public string Name;
+    public int HighScore;
+
+    //singleton stuff
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    //methods for updating the name and the score
+    public void NewNameCreated (string name)
+    {
+        MenuManager.Instance.Name = name;
+    }
+
+    public void UpdateHighScore(int highScore)
+    {
+        MenuManager.Instance.HighScore = highScore;
+    }
+
+
+    //save data class (put everything together)
+    [System.Serializable]
+    class SaveData
+    {
+        public string name;
+        public int highScore;
+    }
+    //method for putting the data into json file
+
+    public void SaveNameScore()
+    {
+        SaveData data = new SaveData();
+        data.name = Name;
+        data.highScore = HighScore;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    //extracting the data back into the ol' mighty singleton
+    public void LoadNameScore()
+    {
+        string path = Application.persistentDataPath + "savefile.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            Name = data.name;
+            HighScore = data.highScore;
+
+        }
+    }
 
 
 
 
 
-
-
-
-
-
+    //scene management part (should've been a separate script)
     public void StartGame()
     {
         SceneManager.LoadScene(1);
